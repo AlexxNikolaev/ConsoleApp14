ba11028adacb0fb40d13cb651d743a97e327b81e
AlexxNikolaev <143645546+AlexxNikolaev@users.noreply.github.com>
2023-12-21 22:27:05 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SumBetweenConstants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int X = 5;
+            const int Y = 10;
+
+            int sum = CalculateSumBetween(X, Y);
+
+            Console.WriteLine($"Sum of numbers between {X} and {Y} is: {sum}");
+        }
+
+        static int CalculateSumBetween(int start, int end)
+        {
+            if (start == end)
+            {
+                return start;
+            }
+  
+            int sum = 0;
+      
+            for (int i = Math.Min(start, end); i <= Math.Max(start, end); i++)
+            {
+                sum += i;
+            }
+
+            return sum;
+        }
+    }
+}
